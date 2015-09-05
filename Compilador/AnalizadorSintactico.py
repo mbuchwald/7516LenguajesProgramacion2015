e8d4d81c0a6f4128f7dbd5e39d71e69ed145ba60
Martin Buchwald <martine9111@gmail.com>
2015-09-05 16:14:17 -0300

    Agregada primera version de analisis sintactico de declaracion de variables y procedimientos

--- Compilador/AnalizadorSintactico.py
+++ Compilador/AnalizadorSintactico.py
@@ -30,7 +30,7 @@ class AnalizadorSintactico(object):
 							
 							simbolo = self.scanner.obtener_simbolo()
 							if simbolo == AnalizadorLexico.PUNTO_Y_COMA:
-								self.scanner.obtener_simbolo()
+								simbolo = self.scanner.obtener_simbolo()
 								break
 							elif simbolo != AnalizadorLexico.COMA:
 								output.write("Error Sintactico: Se esperaba punto y coma (;) o coma (,) luego de declaracion de constante\n")
@@ -44,7 +44,41 @@ class AnalizadorSintactico(object):
 				else:
 					output.write("Error Sintactico: declaracion de constante no seguida de un identificador\n")
 					break
-		
+					
+		valor = self.scanner.obtener_valor_actual()
+		if simbolo == AnalizadorLexico.IDENTIFICADOR_O_RESERVADA and valor == VAR:
+			while True:
+				simbolo = self.scanner.obtener_simbolo()
+				if simbolo == AnalizadorLexico.IDENTIFICADOR_O_RESERVADA:
+					identificador = self.scanner.obtener_valor_actual()
+					#TODO: definir identificador como variable, analizador que no sea reservada ni constante ni variable, etc...
+					if simbolo == AnalizadorLexico.PUNTO_Y_COMA:
+						simbolo = self.scanner.obtener_simbolo()
+						break
+					elif simbolo != AnalizadorLexico.COMA:
+						output.write("Error Sintactico: Se esperaba punto y coma (;) o coma (,) luego de declaracion de variable\n")
+						break
+				else:
+					output.write("Error Sintactico: declaracion de variable no seguida de un identificador\n")
+					break
+							
+		while simbolo == AnalizadorLexico.IDENTIFICADOR_O_RESERVADA and self.scanner.obtener_valor_actual() == PROCEDURE:
+			simbolo = self.scanner.obtener_simbolo()
+			if simbolo != AnalizadorLexico.IDENTIFICADOR_O_RESERVADA:
+				output.write("Error Sintactico: declaracion de procedimiento no seguida de un identificador\n")
+				continue
+			identificador = self.scanner.obtener_valor_actual()
+			#TODO: definir identificador como procedimiento + validaciones
+			simbolo = self.scanner.obtener_simbolo()
+			if simbolo != AnalizadorLexico.PUNTO_Y_COMA:
+				output.write("Error Sintactico: Luego de la identificacion de un procedimiento se esperaba por punto y coma (;)\n")
+				continue
+			self._parsear_bloque() #ver como hacer para que quede asociado al identificador
+			if self.scanner.obtener_tipo_actual() != AnalizadorLexico.PUNTO_Y_COMA:
+				output.write("Luego de definir un procedimiento se esperaba por punto y coma (;)\n")
+				continue
+			simbolo = self.scanner.obtener_simbolo()
+			
 	
 	def parsear_programa(self):
 		self._parsear_bloque()
